6d4cdc6a6e9509a80bceeeb25cbe33ab964a87e6
cwnidog <cwnidog@comcast.net>
2015-08-04 16:15:09 -0700

    Initial project files.

---
+++ VowelMovement/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  VowelMovement
+//
+//  Created by John Leonard on 8/4/15.
+//  Copyright (c) 2015 John Leonard. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+    return 0;
+}
